b6b56be7b80c22d094f9e7632f6cc64b3ae46883
harishreddypothula <write2phreddy@gmail.com>
2019-08-12 12:57:56 -0700

    Adding simple mbuttons to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'bob', loadChildren: './bob/bob.module#BobModule'},
+  {path: 'heather', loadChildren: './heather/heather.module#HeatherModule'},
+  {path: 'alex', loadChildren: './alex/alex.module#AlexModule'},
+  {path: 'tim', loadChildren: './tim/tim.module#TimModule'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
